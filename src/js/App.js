3676d904e0a28f62c8947851aaa2bf84add87ab1
Liang <hlliang@amazon.com>
2015-03-31 11:21:34 -0700

    fixed styles and stuff

--- src/js/App.js
+++ src/js/App.js
@@ -3,18 +3,21 @@ var React = require('react');
 var CardSelector = require('./components/CardSelector.js');
 
 React.render(
-    <div>
-        <h1>RNGenie</h1>
-        <CardSelector
-         description="Search Collectible Minions By Name: "
-         precondition={function(x) {
-            return (x.type === "Minion") && (x.collectible);
-        }} />
-        <CardSelector
-         description="Search Collectible Spells By Name: "
-         precondition={function(x) {
-            return (x.type === "Spell") && (x.collectible);
-        }} />
+    <div className="container-fluid">
+        <div className="col-md-6">
+            <CardSelector
+             description="Search Collectible Minions By Name: "
+             precondition={function(x) {
+                return (x.type === "Minion") && (x.collectible);
+            }} />
+        </div>
+        <div className="col-md-6">
+            <CardSelector
+             description="Search Collectible Spells By Name: "
+             precondition={function(x) {
+                return (x.type === "Spell") && (x.collectible);
+            }} />
+        </div>
     </div>,
     document.getElementById('react-mount-point')
 );

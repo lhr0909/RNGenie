d7be87516e398f79f92fa3f5f7d14d081421c093
Liang <hlliang@amazon.com>
2015-04-22 17:23:53 -0700

    working on minion mana selector

--- src/js/App.js
+++ src/js/App.js
@@ -1,22 +1,30 @@
 var React = require('react');
 
 var CardSelector = require('./components/CardSelector.js');
+var MinionManaSelector = require('./components/MinionManaSelector.js');
 
 React.render(
     <div className="container-fluid">
+        <div className="col-md-12">
+            <MinionManaSelector />
+        </div>
         <div className="col-md-6">
             <CardSelector
              description="Search Collectible Minions By Name: "
-             precondition={function(x) {
-                return (x.type === "Minion") && (x.collectible);
-            }} />
+             precondition={
+                function(x) {
+                    return (x.type === "Minion") && (x.collectible);
+                }
+             } />
         </div>
         <div className="col-md-6">
             <CardSelector
              description="Search Collectible Spells By Name: "
-             precondition={function(x) {
-                return (x.type === "Spell") && (x.collectible);
-            }} />
+             precondition={
+                function(x) {
+                    return (x.type === "Spell") && (x.collectible);
+                }
+             } />
         </div>
     </div>,
     document.getElementById('react-mount-point')
